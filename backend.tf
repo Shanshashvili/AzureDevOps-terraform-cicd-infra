a88ab2095bb084de116e399295d294c486110ecf
Davit Shanshashvili <163526401+Shanshashvili@users.noreply.github.com>
2025-03-10 03:17:41 +0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-adolearn-epam-cloud-and-devops-practice-tfstate"
-    storage_account_name = "adolearntfstatestorag"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.42.0"
+    }
   }
+  required_version = ">= 1.3.8"
+  backend "azurerm" {}
 }
